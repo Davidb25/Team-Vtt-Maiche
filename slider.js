f3f348ebdaea68a5d0ec4bb905359e20e2ef8403
Davidb25 <82676295+Davidb25@users.noreply.github.com>
2021-10-30 11:04:30 +0100

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,63 @@
+/* Tuto pour slider images avec transition automatique ou manuel */
+/* https://www.youtube.com/watch?v=REOWRuPpxQE */
+
+
+window.onload = () => {
+    let cpt = 0;
+    let left = document.querySelector('.left');
+    let right = document.querySelector('.right');
+    let contenu = document.querySelector('.contenu');
+    let slider = document.querySelector('.slider');
+
+    let defilement = Array.from(contenu.children);
+    let screenwidth = slider.getBoundingClientRect().width;
+    right.addEventListener('click', avance);
+    left.addEventListener('click', recule);
+    window. onresize = recentrage; /*fonction quand on redimensionne la fenêtre*/
+    setInterval(avance,6000)
+    console.log(screenwidth);
+
+    function avance() {
+    let screenwidth = slider.getBoundingClientRect().width;
+    cpt++;
+    if (cpt === defilement.length) {
+        cpt = 0;
+    }
+    let decalage = -screenwidth * cpt;
+    contenu.style.transform=`translateX(${ decalage }px)`;
+    /*alert('avance d'une image')*/
+    /*console.log(cpt);*/
+    /*console.log(defilement); */
+    }
+
+
+    function recule() {
+    let screenwidth = slider.getBoundingClientRect().width;
+    cpt--;
+    if (cpt < 0) {
+        cpt = defilement.length - 1;
+    }
+    let decalage = -screenwidth * cpt;
+    contenu.style.transform=`translateX(${ decalage }px)`;
+    /*console.log(cpt)*/
+    /*alert('recule d'une image')*/
+    console.log(screenwidth);
+    }
+
+    function recentrage() {
+        let screenwidth = slider.getBoundingClientRect().width;
+        /*cpt++;*/
+        if (cpt === defilement.length) {
+            cpt = 0;
+        }
+        let decalage = -screenwidth * cpt;
+        contenu.style.transform=`translateX(${ decalage }px)`;
+        /*alert('avance d'une image')*/
+        /*console.log(cpt);*/
+        /*console.log(defilement); */
+        }
+    
+
+
+      
+}
