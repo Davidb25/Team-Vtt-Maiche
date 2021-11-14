c26de7eb2566227245351810055d561c34f227f0
Davidb25 <82676295+Davidb25@users.noreply.github.com>
2021-11-14 16:11:15 +0100

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,33 @@
+//popup
+
+const images = document.querySelectorAll('.image');
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.close-btn');
+const largeImage = document.querySelector('.large-image');
+
+
+images.forEach((i) => {
+    i.addEventListener('click', () => {
+        let source_image = i;
+        console.log(source_image);
+
+        /*largeImage.src = "img/actualites/capture_accueil_site_web.jpg"*/
+
+        largeImage.src = source_image.src;
+
+        popup.classList.toggle('active');
+        document.body.style.overflow = 'visible';
+
+    })
+
+})
+
+closeBtn.addEventListener('click', () => {
+    popup.classList.toggle('active');
+    document.body.style.overflow = 'visible';
+})
+
+
+
+
+
